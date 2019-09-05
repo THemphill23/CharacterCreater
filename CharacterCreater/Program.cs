bac823586eaf59380cc4b701525cd0004e851ed4
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-05 15:21:17 -0400

    Minor Changes

--- CharacterCreater/Program.cs
+++ CharacterCreater/Program.cs
@@ -14,6 +14,12 @@ namespace CharacterCreater
             string charName = Console.ReadLine();
             Console.WriteLine();
 
+            Console.WriteLine();
+            Console.WriteLine("Choose your background:");
+            Console.WriteLine("Noble Streetborn Wild");
+            string backGround = Console.ReadLine();
+            Console.WriteLine();
+
             Console.WriteLine("Choose Your Class:");
             Console.WriteLine("Fighter Mage Rouge");
             string className = Console.ReadLine();
@@ -22,76 +28,86 @@ namespace CharacterCreater
             {
                 case "Fighter":
                     Console.WriteLine();
-                    Console.WriteLine("Choose your background");
+                    Console.WriteLine("Choose your specilization");
                     Console.WriteLine("Soldier Mercenary Chosen");
-                    string backFighter = Console.ReadLine();
-                    bool backSoldier = backFighter.Equals("Soldier");
-                    bool backChosen = backFighter.Equals("Chosen");
-                    if (backSoldier)
+                    string specFighter = Console.ReadLine();
+                    bool specSoldier = specFighter.Equals("Soldier");
+                    bool specChosen = specFighter.Equals("Chosen");
+                    if (specSoldier)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your weapon is a Sword and Shield.");
                     }
-                    else if (backChosen)
+                    else if (specChosen)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your weapon is a Hammer.");
                     }
                     else
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your weapon is a Long sword");
                     }
                     break;
 
                 case "Rouge":
                     Console.WriteLine();
-                    Console.WriteLine("Choose your background:");
+                    Console.WriteLine("Choose your specialization:");
                     Console.WriteLine("Thief Assassin Archer");
-                    string backRouge = Console.ReadLine();
-                    bool backThief = backRouge.Equals("Thief");
-                    bool backArcher = backRouge.Equals("Archer");
+                    string specRouge = Console.ReadLine();
+                    bool specThief = specRouge.Equals("Thief");
+                    bool specArcher = specRouge.Equals("Archer");
                 
-                    if (backThief)
+                    if (specThief)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your weapon is a Whip.");
                     }
-                    else if (backArcher)
+                    else if (specArcher)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("You weapon is a Bow and Arrows.");
                     }
                     else
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your weapon is a Dagger");
                     }
                     break;
 
                 case "Mage":
                     Console.WriteLine();
-                    Console.WriteLine("Choose your background");
+                    Console.WriteLine("Choose your specialization");
                     Console.WriteLine("Sorcerer Wizard Enchanter");
-                    string backMage = Console.ReadLine();
-                    bool backSorcerer = backMage.Equals("Sorcerer");
-                    bool backEnchanter = backMage.Equals("Enchanter");
-                    if (backSorcerer)
+                    string specMage = Console.ReadLine();
+                    bool specSorcerer = specMage.Equals("Sorcerer");
+                    bool specEnchanter = specMage.Equals("Enchanter");
+                    if (specSorcerer)
                     {
-                        Console.WriteLine("Your have inate magical powers.");
+                        Console.WriteLine();
+                        Console.WriteLine("Your were born with magical powers.");
                     }
-                    else if (backEnchanter)
+                    else if (specEnchanter)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your magical powers come from a magical item.");
                     }
                     else
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your studied to gain magical powers.");
                     }
                     break;
 
                 default:
+                    Console.WriteLine();
                     Console.WriteLine("Good Luck");
                     break;
             }
           
 
             Console.WriteLine();
-            Console.WriteLine(charName + " the " + className);
+            Console.WriteLine(charName + " the " + backGround + " " + className);
             Console.WriteLine("Enjoy your Journey");
         }
     }
