00aaebe57796364bfa534939cf1570894e190dac
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-05 11:45:03 -0400

    Character Creater

---
+++ CharacterCreater/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CharacterCreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Adventurer");
+            Console.WriteLine();
+
+            
+            Console.WriteLine("Add Character Name:");
+            string charName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Choose Your Class:");
+            Console.WriteLine("Fighter Mage Rouge");
+            string className = Console.ReadLine();
+
+            
+            if (className.Equals("Rouge"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Choose you background:");
+                Console.WriteLine("Thief Assassin");
+                string proName = Console.ReadLine();
+            }
+
+            
+
+            Console.WriteLine();
+            Console.WriteLine(charName + " the " + className);
+            Console.WriteLine("Enjoy your Journey");
+        }
+    }
+}
