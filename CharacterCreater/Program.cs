3e4134ea472e5dba3ba70dac32fb4e0a1ee2f875
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-05 12:36:15 -0400

    Added Backgrounds and Weapons

--- CharacterCreater/Program.cs
+++ CharacterCreater/Program.cs
@@ -18,16 +18,42 @@ namespace CharacterCreater
             Console.WriteLine("Fighter Mage Rouge");
             string className = Console.ReadLine();
 
-            
-            if (className.Equals("Rouge"))
+            switch (className)
             {
-                Console.WriteLine();
-                Console.WriteLine("Choose you background:");
-                Console.WriteLine("Thief Assassin");
-                string proName = Console.ReadLine();
-            }
+                case "Fighter":
+                    Console.WriteLine();
+                    Console.WriteLine("Choose your background");
+                    Console.WriteLine("Soldier Mercenary");
+                    string backFighter = Console.ReadLine();
+                    bool backSoldier = backFighter.Equals("Soldier");
+                    if (backSoldier)
+                    {
+                        Console.WriteLine("Your weapon is a Sword and Shield.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your weapon is a Long sword");
+                    }
+                    break;
 
-            
+                case "Rouge":
+                    Console.WriteLine();
+                    Console.WriteLine("Choose your background:");
+                    Console.WriteLine("Thief Assassin");
+                    string backRouge = Console.ReadLine();
+                    break;
+
+                case "Mage":
+                    Console.WriteLine();
+                    Console.WriteLine("Choose your background");
+                    Console.WriteLine("Sorcerer Wizard");
+                    string backMage = Console.ReadLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Good Luck");
+                    break;
+            }
 
             Console.WriteLine();
             Console.WriteLine(charName + " the " + className);
