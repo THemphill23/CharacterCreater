31ab6dcab921964dd75f8eef167db8331be813c9
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-05 21:03:12 -0400

    More description and detail

--- CharacterCreater/Program.cs
+++ CharacterCreater/Program.cs
@@ -16,7 +16,7 @@ namespace CharacterCreater
 
             Console.WriteLine();
             Console.WriteLine("Choose your background:");
-            Console.WriteLine("Noble, Streetborn, Wild");
+            Console.WriteLine("Noble, Street Kid, Wild");
             string backGround = Console.ReadLine();
             Console.WriteLine();
 
@@ -27,7 +27,7 @@ namespace CharacterCreater
                     Console.WriteLine("Once adulthood was reach, you were encouraged to explore beyond the walls of the castle.");
                     Console.WriteLine();
                     break;
-                case "Streetborn":
+                case "Street Kid":
                     Console.WriteLine("Born to a poor family, you survived in the city with whatever you could get.");
                     Console.WriteLine("Once you reached adulthood, you decide to explore beyond the city.");
                     Console.WriteLine();
@@ -45,6 +45,7 @@ namespace CharacterCreater
             switch (className)
             {
                 case "Fighter":
+                    Console.WriteLine("Fighter: Straight into the fray attacker.");
                     Console.WriteLine();
                     Console.WriteLine("Choose your specilization");
                     Console.WriteLine("Soldier, Mercenary, Chosen");
@@ -74,6 +75,7 @@ namespace CharacterCreater
                     break;
 
                 case "Rouge":
+                    Console.WriteLine("Rouge: Uses stealth and distance to deal damage.");
                     Console.WriteLine();
                     Console.WriteLine("Choose your specialization:");
                     Console.WriteLine("Thief, Assassin, Archer");
@@ -84,21 +86,28 @@ namespace CharacterCreater
                     if (specThief)
                     {
                         Console.WriteLine();
+                        Console.WriteLine("You lived by stealing and taking what you need.");
+                        Console.WriteLine("That skill translated into a way to make a living.");
                         Console.WriteLine("Your weapon is a Whip.");
                     }
                     else if (specArcher)
                     {
                         Console.WriteLine();
+                        Console.WriteLine("You trained in sharpshooting and fighting from a distance.");
+                        Console.WriteLine("This ability lead to many opportunities for you.");
                         Console.WriteLine("You weapon is a Bow and Arrows.");
                     }
                     else
                     {
                         Console.WriteLine();
+                        Console.WriteLine("You were trained by a secret society to become a master killer.");
+                        Console.WriteLine("You became good at what you do, which has lead to many jobs.");
                         Console.WriteLine("Your weapon is a Dagger");
                     }
                     break;
 
                 case "Mage":
+                    Console.WriteLine("Mage: Uses magic to deal damage and control the field.");
                     Console.WriteLine();
                     Console.WriteLine("Choose your specialization");
                     Console.WriteLine("Sorcerer, Wizard, Enchanter");
@@ -109,16 +118,22 @@ namespace CharacterCreater
                     {
                         Console.WriteLine();
                         Console.WriteLine("Your were born with magical powers.");
+                        Console.WriteLine("You sent time training to harness you power.");
+                        Console.WriteLine("Once you grew up, you took your magic into the world.");
                     }
                     else if (specEnchanter)
                     {
                         Console.WriteLine();
                         Console.WriteLine("Your magical powers come from a magical item.");
+                        Console.WriteLine("You received a fammily heirloom at a young age.");
+                        Console.WriteLine("Once you determine the magical power of the heirloom, you thought to use it.");
                     }
                     else
                     {
                         Console.WriteLine();
                         Console.WriteLine("Your studied to gain magical powers.");
+                        Console.WriteLine("After years of schooling, you became a powerful magic wielder.");
+                        Console.WriteLine("You have taken this magic and left school to see what's out in the world.");
                     }
                     break;
 
